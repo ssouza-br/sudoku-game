6e8f531cc25922c7f6544c27ba4625986ff3d97b
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 14:47:23 +0000

    Sun, Jun 19, 2022, 2:47 PM +00:00

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,22 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-long int gen_tens(int n);
+long int gen_decimals(int n);
 
 int main(void)
 {
     long int n = get_long("Number: ");
-    printf("%li\n", gen_tens(n));
+    printf("%li\n", gen_decimals(n));
     // printf("%li\n", n);
     // printf("%li\n", n%10);
     // printf("%li\n", (n%100- n%10)/10);
     // printf("%li\n", (n%1000 - n%100)/100);
     // printf("%li\n", (n%10000 - n%1000)/1000);
-
-
 }
 
-long int gen_tens(int n)
+long int gen_decimals(int n)
 {
     long int res = 1;
     int i = n;
