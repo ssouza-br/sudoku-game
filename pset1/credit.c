2150618ae6c3e0f54e7bacaf91e7e556b84df6ea
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 16:42:48 +0000

    Sun, Jun 19, 2022, 4:42 PM +00:00

--- pset1/credit.c
+++ pset1/credit.c
@@ -5,7 +5,7 @@ long int gen_decimals(int n);
 int get_digits(long int n, int pos);
 int len_number(long int n);
 string card_type(long int n);
-int check_sum(long int n);
+int check_sum_part1(long int n);
 
 int main(void)
 {
@@ -50,10 +50,31 @@ int len_number(long int n)
     return i - 1;
 }
 
-int check_sum(long int n)
+int check_sum_part1(long int n)
 {
     int sum = 0;
     for (int i = len_number(n); i > 0;i=i-2)
+    {
+        int double_digts = 2*get_digits(n, i);
+        if (len_number(double_digts) > 1)
+        {
+            sum = sum + get_digits(double_digts, 1) + get_digits(double_digts, 2);
+        }
+        else{
+            sum = sum + double_digts;
+        }
+
+        printf("position: %i\n", i);
+        printf("digito: %i\n", get_digits(n, i));
+        printf("item soma: %i\n", 2*get_digits(n, i));
+    }
+    return sum;
+}
+
+int check_sum_part2(long int n)
+{
+    int sum = 0;
+    for (int i = len_number(n) - 1; i > 0;i=i-2)
     {
         int double_digts = 2*get_digits(n, i);
         if (len_number(double_digts) > 1)
