35006d0aed31e980a6bd762bae463ce745361127
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 01:42:44 +0000

    Sun, Jun 19, 2022, 1:42 AM +00:00

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,10 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void horizontal_block(int n);
+
 int main(void){
-    printf("#  #\n");
+    //printf("#  #\n");
+    horizontal_block(2);
 }
 
 void horizontal_block(int n){
-    
+    string ptr = "##";
+    printf("%s",3*ptr);
 }
