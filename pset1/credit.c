f79eb599701145a392272a247d3421c54f4a3e15
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 13:25:47 +0000

    Sun, Jun 19, 2022, 1:25 PM +00:00

---
+++ pset1/credit.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    printf("Hello, %s\n", name);
+}
+
+string luhns_algorithm(long int n)
+{
+    
+}
