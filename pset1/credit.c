23c27c480e4df92b6d2c088f12ff04142fe53905
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 14:29:59 +0000

    Sun, Jun 19, 2022, 2:29 PM +00:00

--- pset1/credit.c
+++ pset1/credit.c
@@ -6,9 +6,14 @@ int main(void)
     long int n = get_long("Number: ");
     printf("%li\n", n);
     printf("%li\n", n%10);
-    printf("%li\n", n%100- n%10);
-    printf("%li\n", n%1000 - n%100);
+    printf("%li\n", (n%100- n%10)/10);
+    printf("%li\n", (n%1000 - n%100)/100);
 
+}
+
+int get_digits(long int n)
+{
+    
 }
 
 // string luhns_algorithm(long int n)
