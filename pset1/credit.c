129f85468308f304c195424921e04f733614f551
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 17:02:10 +0000

    Sun, Jun 19, 2022, 5:02 PM +00:00

--- pset1/credit.c
+++ pset1/credit.c
@@ -12,10 +12,10 @@ bool card_validation(long int n);
 int main(void)
 {
     long int n = get_long("Number: ");
-    printf("%s\n",card_type(n));
+    //printf("%s\n",card_type(n));
     // printf("%i\n",check_sum_part1(n));
     // printf("%i\n",check_sum_part2(n));
-    // printf("%s", card_validation(n) ? "true" : "false");
+    printf("%s", card_validation(n) ? "true" : "false");
    // int pos = get_int("position: ");
    // printf("%i\n", get_digits(n,pos));
     // printf("%li\n", n);
