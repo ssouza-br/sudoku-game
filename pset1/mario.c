4d5464b406de5ed495417089c648d226fb668732
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 13:10:33 +0000

    Sun, Jun 19, 2022, 1:10 PM +00:00

--- pset1/mario.c
+++ pset1/mario.c
@@ -3,12 +3,16 @@
 
 void horizontal_block(int n, int m);
 
-int main(void){
+int main(void)
+{
     int m;
-    do{
+    do
+    {
          m = get_int("Height: ");
-        if(m<=8 && m>0){
-            for(int i = 1;i<=m;i++){
+        if(m<=8 && m>0)
+        {
+            for(int i = 1;i<=m;i++)
+            {
                 horizontal_block(i,m);
                 printf("\n");
             }
@@ -17,20 +21,21 @@ int main(void){
     while(m<=0 || m>8);
 }
 
-void parts_block(int n){
-    for(int i = 0;i<n;i++){
+void parts_block(int n)
+{
+    for(int i = 0;i<n;i++)
         printf("#");
-    }
 }
 
-void parts_space(int n){
-    for(int i = 0;i<n;i++){
+void parts_space(int n)
+{
+    for(int i = 0;i<n;i++)
         printf(" ");
-    }
 }
 
-void horizontal_block(int n, int m){
-    parts_space(m-n);
+void horizontal_block(int n, int m)
+{
+    parts_space(m - n);
     parts_block(n);
     printf("  ");
     parts_block(n);
