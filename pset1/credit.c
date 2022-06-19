10f4984cb6a7fd1eba1702c1bb37b80c1615546d
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-19 14:31:33 +0000

    Sun, Jun 19, 2022, 2:31 PM +00:00

--- pset1/credit.c
+++ pset1/credit.c
@@ -8,13 +8,14 @@ int main(void)
     printf("%li\n", n%10);
     printf("%li\n", (n%100- n%10)/10);
     printf("%li\n", (n%1000 - n%100)/100);
+    printf("%i\n", 10^2)
 
 }
 
-int get_digits(long int n)
-{
-    
-}
+// int get_digits(long int n)
+// {
+
+// }
 
 // string luhns_algorithm(long int n)
 // {
