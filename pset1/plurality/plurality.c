5a6ebde58d24a2bf640da4b99ee49b7c006f47e5
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 21:11:02 +0000

    Mon, Jun 20, 2022, 9:11 PM +00:00

--- pset1/plurality/plurality.c
+++ pset1/plurality/plurality.c
@@ -68,7 +68,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].name == name)
         {
