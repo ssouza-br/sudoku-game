a1fd1522947724caca386858bef87d610918f8b8
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 23:03:29 +0000

    Mon, Jun 20, 2022, 11:03 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%i\n", count_letters(text));
+    printf("%i letters\n", count_letters(text));
+    printf("%i words\n", count_words(text));
 }
 
 int count_letters(string text)
@@ -27,14 +29,14 @@ int count_letters(string text)
 int count_words(string text)
 {
     int len = strlen(text);
-    int counter_letters = 0;
+    int counter_words = 0;
     for (int i = 0; i < len; i++)
     {
-        if (isalpha(text[i]))
+        if (strcmp(text[i], " ") == 0)
         {
-            counter_letters++;
+            counter_words++;
         }
     }
-    return counter_letters;
+    return counter_words;
 }
 
