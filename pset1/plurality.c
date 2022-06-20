d90d178f9c3a563a3ded62469eccc1ddaa2e8bd1
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 20:48:35 +0000

    Mon, Jun 20, 2022, 8:48 PM +00:00

--- pset1/plurality.c
+++ pset1/plurality.c
@@ -1,18 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+typedef struct // Cria uma STRUCT para armazenar os dados de uma pessoa
+{
+    string name;   // define o campo Peso
+    int votes;    // define o campo Idade
+} candidate; // Define o nome do novo tipo criado
+
+candidate cadidates[10];
+
 int main(void)
 {
     int votes_number = get_int("Number of voters: ");
-
-    typedef struct // Cria uma STRUCT para armazenar os dados de uma pessoa
-    {
-        string name;   // define o campo Peso
-        int votes;    // define o campo Idade
-    } candidate; // Define o nome do novo tipo criado
-
-    candidate cadidates[10]
-    
 }
 
 
