848a8c829dbfeecc63a69f364275fec9159d0111
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 22:54:50 +0000

    Mon, Jun 20, 2022, 10:54 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,18 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+int count_letters(string text);
 
 int main(void)
 {
-    string text = get_string("Text: ")
+    string text = get_string("Text: ");
+    printf("%i\n", count_letters(text));
 }
 
 int count_letters(string text)
 {
     int len = strlen(text);
     int counter_letters = 0;
-    for (i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        if (text[i] = )
+        if (isalpha(text[i]))
+        {
+            counter_letters++;
+        }
     }
 }
