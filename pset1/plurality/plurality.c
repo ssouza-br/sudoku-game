cc3a4c2af47c99b741e78588b45f7401ba653312
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 21:49:07 +0000

    Mon, Jun 20, 2022, 9:49 PM +00:00

--- pset1/plurality/plurality.c
+++ pset1/plurality/plurality.c
@@ -75,7 +75,7 @@ bool vote(string name)
     {
         if (strcmp(candidates[i].name,name))
         {
-            candidates[i].votes += 1;
+            candidates[i].votes++;
             return true;
         }
     }
