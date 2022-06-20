dc4c56c35d443d0e5e127b445174081837adfb1a
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 23:18:10 +0000

    Mon, Jun 20, 2022, 11:18 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -11,7 +11,7 @@ int main(void)
     string text = get_string("Text: ");
     printf("%i letters\n", count_letters(text));
     printf("%i words\n", count_words(text));
-    printf("%i words\n", count_sentences(text));
+    printf("%i sentences\n", count_sentences(text));
 }
 
 int count_letters(string text)
