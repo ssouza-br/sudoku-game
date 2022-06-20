47e176e24900139eaa738c93f21cb9a6900461fe
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-20 21:13:47 +0000

    Mon, Jun 20, 2022, 9:13 PM +00:00

--- pset1/plurality/plurality.c
+++ pset1/plurality/plurality.c
@@ -48,6 +48,8 @@ int main(int argc, string argv[])
     int voter_count = get_int("Number of voters: ");
     printf("%s\n",candidates[0].name);
     printf("%s\n",candidates[1].name);
+    printf("%i\n",(candidates[1].name == "Bob"));
+    printf("%i\n",(candidates[0].name == "Alice"));
 
     // Loop over all voters
     for (int i = 0; i < voter_count; i++)
