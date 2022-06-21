3ce9dfecb904e6a33eac63b330d788ec09bd006a
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-21 01:45:42 +0000

    Tue, Jun 21, 2022, 1:45 AM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -18,7 +18,7 @@ string encrypt(string text, string key)
     string str_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int len = strlen(text);
     char new_text[len];
-    char return_text[200];
+    char return_text[len];
 
     for (int i = 0; i < 26; i++)
     {
@@ -36,6 +36,7 @@ string encrypt(string text, string key)
             }
         }
     }
-    //strcpy(return_text, new_text);
-    return return_text;
+    strcpy(return_text, new_text);
+    printf("%s\n", return_text);
+    return text;
 }
