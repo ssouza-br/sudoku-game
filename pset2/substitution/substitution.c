3b1169dd6470d10c7c7fe5f19f37f0531f8e679d
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-21 00:51:29 +0000

    Tue, Jun 21, 2022, 12:51 AM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -1,9 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     string key = argv[1]
 }
 
-string 
+string encrypt(string text, string key)
+{
+    char decode[26];
+    for (int i =0; i < 26; i++)
+    {
+        decode[i] = key[i];
+    }
+}
