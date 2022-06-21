20a47a50ee8207a43f1c2fbbecf6b8f2ab73de68
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-21 01:56:29 +0000

    Tue, Jun 21, 2022, 1:56 AM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -34,10 +34,6 @@ void encrypt(string text, string key)
             {
                 new_text[i] = decode[j];
             }
-            else
-            {
-                 new_text[i] = text[i];
-            }
         }
     }
     strcpy(return_text, new_text);
