8660142ee4fcf64681e499089ac4077a75c6e286
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-24 14:43:23 +0000

    Fri, Jun 24, 2022, 2:43 PM +00:00

--- pset3/tideman.c
+++ pset3/tideman.c
@@ -115,7 +115,13 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for ()
+    {
+        for ()
+        {
+            
+        }
+    }
     return;
 }
 
