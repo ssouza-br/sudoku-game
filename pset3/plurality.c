4943dfdee5a920eec0ee61cc65f599b110971330
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-24 00:43:15 +0000

    Fri, Jun 24, 2022, 12:43 AM +00:00

--- pset3/plurality.c
+++ pset3/plurality.c
@@ -73,7 +73,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates[i].name,name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes = candidates[i].votes + 1;
             return true;
