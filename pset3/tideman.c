809e07644831bc6c00c8c9c4736759bf1f6edb31
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-24 21:06:12 +0000

    Fri, Jun 24, 2022, 9:06 PM +00:00

--- pset3/tideman.c
+++ pset3/tideman.c
@@ -132,6 +132,14 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
+    for ()
+    {
+        for ()
+        {
+            
+        }
+    }
+
     // TODO
     return;
 }
