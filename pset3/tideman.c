7ce12b6fa71ec95d91a7cdc1ac42ba6eab9f9901
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-24 21:56:48 +0000

    Fri, Jun 24, 2022, 9:56 PM +00:00

--- pset3/tideman.c
+++ pset3/tideman.c
@@ -162,12 +162,15 @@ void sort_pairs(void)
         if (strength > max_strength)
         {
             max_strength = strength;
-            pairs[0] = temp;
-            printf("pairs0 original winner: %i loser: %i", temp.winner, temp.loser);
-            pairs[0] = pairs[i];
-            printf("pairs0 atual winner: %i loser: %i", pairs[0].winner, pairs[0].loser);
-            pairs[i] = temp;
-            printf("pairsi trocado winner: %i loser: %i", pairs[i].winner, pairs[i].loser);
+            temp.winner = pairs[0].winner;
+            temp.loser =  pairs[0].loser;
+            //printf("pairs0 original winner: %i loser: %i", temp.winner, temp.loser);
+            pairs[0].winner = pairs[i].winner;
+            pairs[0].loser = pairs[i].loser;
+            //printf("pairs0 atual winner: %i loser: %i", pairs[0].winner, pairs[0].loser);
+            pairs[i].winner = temp.winner;
+            pairs[i].loser = temp.loser;
+            //printf("pairsi trocado winner: %i loser: %i", pairs[i].winner, pairs[i].loser);
         }
     }
 
