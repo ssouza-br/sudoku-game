db9420f564fce790a9e8a5f7ec64eefdbf20e2a2
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-25 00:05:58 +0000

    Sat, Jun 25, 2022, 12:05 AM +00:00

--- pset3/tideman.c
+++ pset3/tideman.c
@@ -173,6 +173,12 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
+    typedef struct
+    {
+        int sair;
+        int recebe;
+    }
+    status;
     for (int i = 0; i < pair_count; i++)
     {
         locked[pairs[i].winner][pairs[i].loser] = true;//caso de n ter cycle
