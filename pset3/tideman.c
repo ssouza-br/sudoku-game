20b154319f23d11e856ad68a51bcdac8e88ad62c
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-25 01:50:02 +0000

    Sat, Jun 25, 2022, 1:50 AM +00:00

--- pset3/tideman.c
+++ pset3/tideman.c
@@ -222,6 +222,17 @@ bool check_source(void)
 // Print the winner of the election
 void print_winner(void)
 {
-
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (locked[i][j] == true)
+            {
+                pairs[k].winner = i;
+                pairs[k].loser = j;
+                k++;
+            }
+        }
+    }
     return;
 }
