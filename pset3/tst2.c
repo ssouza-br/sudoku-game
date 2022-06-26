ffa3545d3ee39e69aaebd44da23fef9ea047350e
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 02:27:12 +0000

    Sun, Jun 26, 2022, 2:27 AM +00:00

--- pset3/tst2.c
+++ pset3/tst2.c
@@ -15,6 +15,9 @@ pair;
 // Array of candidates
 pair pairs[3];
 
+// locked[i][j] means i is locked in over j
+bool locked[3][3];
+
 
 typedef struct
 {
@@ -34,6 +37,15 @@ int main(void)
     pairs[2].winner = 1;
     pairs[2].loser = 2;
 
+     // Clear graph of locked in pairs
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            locked[i][j] = false;
+        }
+    }
+
     for (int i = 0; i < pair_count; i++)
     {
         printf("valor sai: %i valor recebe: %i\n", status_general[i].sai, status_general[i].recebe);
@@ -50,23 +62,23 @@ void lock_pairs(void)
     for (int i = 0; i < pair_count; i++)
     {
         int temp_sai, temp_recebe;
-        printf("par number: %i winner: %i loser: %i\n", i, pairs[i].winner, pairs[i].loser);
+        //printf("par number: %i winner: %i loser: %i\n", i, pairs[i].winner, pairs[i].loser);
         temp_sai = status_general[pairs[i].winner].sai;
         temp_recebe = status_general[pairs[i].loser].recebe;
-        printf("temp_sai: %i temp_recebe: %i\n", temp_sai, temp_recebe);
+        //printf("temp_sai: %i temp_recebe: %i\n", temp_sai, temp_recebe);
         status_general[pairs[i].winner].sai = 1;
         status_general[pairs[i].loser].recebe = 1;
         //printf("TRUE OR FALSE: %i\n", check_source());
         if (check_source())
         {
-            printf("entrei\n");
+            //printf("entrei\n");
             locked[pairs[i].winner][pairs[i].loser] = true;
         }
         else
         {
             status_general[pairs[i].winner].sai = temp_sai;
             status_general[pairs[i].loser].recebe = temp_recebe;
-            printf("i: %i st_sai: %i st_recebe: %i\n", i, status_general[pairs[i].winner].sai, status_general[pairs[i].loser].recebe);
+            //printf("i: %i st_sai: %i st_recebe: %i\n", i, status_general[pairs[i].winner].sai, status_general[pairs[i].loser].recebe);
         }
     }
     return;
@@ -77,7 +89,7 @@ bool check_source(void)
     int k = 0;
     for (int j = 0; j < 3; j++)
     {
-        printf("j: %i recebe: %i sai: %i\n", j, status_general[j].recebe, status_general[j].sai);
+        //printf("j: %i recebe: %i sai: %i\n", j, status_general[j].recebe, status_general[j].sai);
         if (status_general[j].recebe == 1 && status_general[j].sai == 1)
         {
             k++;
@@ -85,12 +97,12 @@ bool check_source(void)
     }
     if (k == 3)
     {
-        printf("k FALSE: %i\n", k);
+        //printf("k FALSE: %i\n", k);
         return false;
     }
     else
     {
-        printf("k TRUE: %i\n", k);
+        //printf("k TRUE: %i\n", k);
         return true;
     }
 }
