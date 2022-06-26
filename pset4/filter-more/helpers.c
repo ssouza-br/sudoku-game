4ad8ce923d39294a0cf99c4fc78aad1e72e70617
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 16:11:22 +0000

    Sun, Jun 26, 2022, 4:11 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -30,7 +30,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     //RGBTRIPLE tmp1, tmp2;
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             tmpBlue1 = image[i][j].rgbtBlue;
             tmpRed1 = image[i][j].rgbtRed;
