1f6bd1ea40e5afcd11beaf21c6a8eb3a497ed3c8
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 14:42:24 +0000

    Sun, Jun 26, 2022, 2:42 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -1,13 +1,23 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int tmpBlue = 0, tmpRed = 0, tmpGreen = 0, avgValue = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].
+            tmpBlue = image[i][j].rgbtBlue;
+            tmpRed = image[i][j].rgbtRed;
+            tmpGreen = image[i][j].rgbtGreen;
+
+            avgValue = round((tmpBlue + tmpRed + tmpGreen) / 3);
+
+            image[i][j].rgbtBlue = avgValue;
+            image[i][j].rgbtRed = avgValue;
+            image[i][j].rgbtGreen = avgValue;
         }
     }
     return;
