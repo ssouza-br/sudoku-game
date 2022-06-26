e4cde4cadd61eeb00c01ec72135e6ccf526af0ea
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 22:28:25 +0000

    Sun, Jun 26, 2022, 10:28 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -85,5 +85,43 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE new_image[height][width];
+    int Gx[3][3], Gy[3][3];
+
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int counter = 0;
+            int tmpBlue = 0, tmpRed = 0, tmpGreen = 0, index_i = 0, index_j = 0;
+            for (int k = -1; k < 2; k++)
+            {
+                for (int m = -1; m < 2; m++)
+                {
+                    index_i = i + k;
+                    index_j = j + m;
+                    if (index_i >= 0 && index_j >= 0 && index_i <= height - 1 && index_j <= width - 1)
+                    {
+                        tmpBlue = tmpBlue + image[index_i][index_j].rgbtBlue;
+                        tmpRed = tmpRed + image[index_i][index_j].rgbtRed;
+                        tmpGreen = tmpGreen + image[index_i][index_j].rgbtGreen;
+                        counter++;
+                    }
+                }
+            }
+            new_image[i][j].rgbtBlue = round((float) tmpBlue / counter);
+            new_image[i][j].rgbtRed = round((float) tmpRed / counter);
+            new_image[i][j].rgbtGreen = round((float) tmpGreen / counter);
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = new_image[i][j];
+        }
+    }
     return;
 }
