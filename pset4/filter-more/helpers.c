4601513a4c7ffc39d1fe12e6576e4fc577411b3e
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 22:10:00 +0000

    Sun, Jun 26, 2022, 10:10 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -66,9 +66,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                 }
             }
-            new_image[i][j].rgbtBlue = round(tmpBlue / counter);
-            new_image[i][j].rgbtRed = round( tmpRed / counter);
-            new_image[i][j].rgbtGreen = round(tmpGreen / counter);
+            new_image[i][j].rgbtBlue = (int) round((float) tmpBlue / counter);
+            new_image[i][j].rgbtRed = (int) round((float) tmpRed / counter);
+            new_image[i][j].rgbtGreen = (int) round((float) tmpGreen / counter);
         }
     }
 
