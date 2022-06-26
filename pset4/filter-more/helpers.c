6dac1c22a09f51c93b20e42496f2061c6f886130
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 22:10:43 +0000

    Sun, Jun 26, 2022, 10:10 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -77,9 +77,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             image[i][j] = new_image[i][j];
-            return;
         }
     }
+    return;
 }
 
 // Detect edges
