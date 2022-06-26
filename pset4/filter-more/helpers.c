940596352d2e397f0130ee422058f7f091dcbb4f
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 16:15:27 +0000

    Sun, Jun 26, 2022, 4:15 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -26,7 +26,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int tmpBlue1 = 0, tmpRed1 = 0, tmpGreen1 = 0, tmpBlue2 = 0, tmpRed2 = 0, tmpGreen2 = 0;
     RGBTRIPLE tmp1, tmp2;
     for (int i = 0; i < height; i++)
     {
@@ -37,21 +36,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
             image[i][j] = tmp2;
             image[i][width - j - 1] = tmp1;
-            // tmpBlue1 = image[i][j].rgbtBlue;
-            // tmpRed1 = image[i][j].rgbtRed;
-            // tmpGreen1 = image[i][j].rgbtGreen;
-
-            // tmpBlue2 = image[i][width - j - 1].rgbtBlue;
-            // tmpRed2 = image[i][width - j- 1].rgbtRed;
-            // tmpGreen2 = image[i][width - j- 1].rgbtGreen;
-
-            // image[i][j].rgbtBlue = tmpBlue2;
-            // image[i][j].rgbtRed = tmpRed2;
-            // image[i][j].rgbtGreen = tmpGreen2;
-
-            // image[i][width - j- 1].rgbtBlue = tmpBlue1;
-            // image[i][width - j- 1].rgbtRed = tmpRed1;
-            // image[i][width - j- 1].rgbtGreen = tmpGreen1;
         }
     }
     return;
