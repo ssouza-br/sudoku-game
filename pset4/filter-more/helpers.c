07e34fd32808f98e3a94a53150ad81587955aa80
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 17:18:57 +0000

    Sun, Jun 26, 2022, 5:18 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -44,17 +44,18 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    int tmpBlue = 0, tmpRed = 0, tmpGreen = 0, counter = 0;
+    int tmpBlue = 0, tmpRed = 0, tmpGreen = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            int counter = 0;
             for (int k = -1; k < 2; k++)
             {
                 for (int m = -1; m < 2; m++)
                 {
                     int index_i = i + k, index_j = j + m;
-                    if (index_i >= 0 && index_j >= 0)
+                    if (index_i >= 0 && index_j >= 0 && index_i <= height - 1 && index_j <= width - 1)
                     {
                         tmpBlue = tmpBlue + image[index_i][index_j].rgbtBlue;
                         tmpRed = tmpRed + image[index_i][index_j].rgbtRed;
@@ -62,7 +63,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
-
             }
             image[i][j].rgbtBlue = round((float) tmpBlue / counter);
             image[i][j].rgbtRed = round((float) tmpRed / counter);
