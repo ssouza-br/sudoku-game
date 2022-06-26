595936b69ecb35e8d4558056a6ea86d604e32e0b
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 17:01:15 +0000

    Sun, Jun 26, 2022, 5:01 PM +00:00

--- pset4/filter-more/tst.c
+++ pset4/filter-more/tst.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    int avg = round(((float) 28 + (float) 27 + (float) 28)/3);
-    //int avg = round(avg2);
-
-    printf("avg: %i", avg);
+for (int m = -1; m < 2; m++)
+{
+    printf("m: %i\n", m);
+}
 }
