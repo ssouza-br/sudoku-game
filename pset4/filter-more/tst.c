dab130f0ad404e10b4b1e25ce0921d3df06e9450
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 14:56:42 +0000

    Sun, Jun 26, 2022, 2:56 PM +00:00

--- pset4/filter-more/tst.c
+++ pset4/filter-more/tst.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    int avg = (28 + 27 + 28)/3;
+    float avg2 = (28 + 27 + 28)/3;
+    int avg = round(avg2);
 
-    printf("avg: %i", avg);
+    printf("avg: %f", avg2);
 }
