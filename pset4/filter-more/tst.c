7a55a5eb75c8f555d44fa0f26416cbab49670b88
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-26 17:03:02 +0000

    Sun, Jun 26, 2022, 5:03 PM +00:00

--- pset4/filter-more/tst.c
+++ pset4/filter-more/tst.c
@@ -3,8 +3,29 @@
 
 int main(void)
 {
-for (int m = -1; m < 2; m++)
-{
-    printf("m: %i\n", m);
-}
+    int tmpBlue = 0, tmpRed = 0, tmpGreen = 0, counter = 0;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int k = -1; k < 2; k++)
+            {
+                for (int m = -1; m < 2; m++)
+                {
+                    int index_i = i + k, index_j = j + m;
+                    if (index_i >= 0 && index_j >= 0)
+                    {
+                        tmpBlue = tmpBlue + image[index_i][index_j].rgbtBlue;
+                        tmpRed = tmpRed + image[index_i][index_j].rgbtRed;
+                        tmpGreen = tmpGreen + image[index_i][index_j].rgbtGreen;
+                        counter++;
+                    }
+                }
+
+            }
+            image[i][j].rgbtBlue = round((float) tmpBlue / counter);
+            image[i][j].rgbtRed = round((float) tmpRed / counter);
+            image[i][j].rgbtGreen = round((float) tmpGreen / counter);
+        }
+    }
 }
