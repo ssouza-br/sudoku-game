46a7e518a7a363ad13c30a7752a397cd56b49f8c
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-27 21:48:09 +0000

    Mon, Jun 27, 2022, 9:48 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-        if ()
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            
+
         }
 
     }
