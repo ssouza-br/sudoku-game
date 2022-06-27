c92dfc2cb9a3fb6390862f30da1c07c261d1b911
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-27 22:04:27 +0000

    Mon, Jun 27, 2022, 10:04 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
             sprintf(filename, "%03i.jpg", counter);
             printf("meu nome de arquivo: %s\n", filename);
             FILE *img = fopen(filename, "w");
+            fwrite(buffer, BLOCK_SIZE, 1, img)
         }
 
     }
