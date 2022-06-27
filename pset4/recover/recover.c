1229eba36bd44ff164c04992748a2cd7a5e2487f
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-27 22:00:26 +0000

    Mon, Jun 27, 2022, 10:00 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -15,16 +15,15 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
 
     BYTE *buffer = malloc(512);
-
+    int counter = -1;
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-        int counter = -1;
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             counter++;
-            char *filename[9];
+            char filename[9];
             sprintf(filename, "%03i.jpg", counter);
-            printf("meu nome de arquivo: %s", filename);
+            printf("meu nome de arquivo: %s\n", filename);
             fopen(filename, "w");
         }
 
