b184b609ec85bc6675d8acdb5190e9779dc9ef0f
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-27 22:41:16 +0000

    Mon, Jun 27, 2022, 10:41 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -19,11 +19,13 @@ int main(int argc, char *argv[])
     char filename[9];
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
+        
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             counter++;
             sprintf(filename, "%03i.jpg", counter);
             printf("meu nome de arquivo: %s\n", filename);
+            fclose(img0);
         }
         FILE *img0 = fopen(filename, "w");
         fwrite(buffer, 1, BLOCK_SIZE, img0);
