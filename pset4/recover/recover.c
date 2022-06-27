d4e4468ea757a5f3ae3a63414a2eb8e7e3290bb8
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-27 21:53:48 +0000

    Mon, Jun 27, 2022, 9:53 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             counter++;
-            char *filename = sprintf()
+            sprintf(filename, "%03i.jpg", counter);
             fopen(filename, "w")
         }
 
