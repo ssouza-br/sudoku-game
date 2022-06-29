83475c06fce0da35d2ad481c0f6874ab6ea4e924
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-29 00:30:01 +0000

    Wed, Jun 29, 2022, 12:30 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -42,7 +42,8 @@ int main(int argc, char *argv[])
         }
         if (pack.type == 1)
         {
-            pack.file = fopen(sprintf(filename, "%03i.jpg", pack.count), "w");
+            sprintf(filename, "%03i.jpg", pack.count);
+            pack.file = fopen(filename, "w");
             fwrite(buffer, 1, BLOCK_SIZE, pack.file);
             pack.count++;
         }
