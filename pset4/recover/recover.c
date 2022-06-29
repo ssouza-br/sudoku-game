c1ed2e8baf94ec0f142d9abf9e37b8c1a98ba4a2
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-06-29 00:07:19 +0000

    Wed, Jun 29, 2022, 12:07 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -36,6 +36,10 @@ int main(int argc, char *argv[])
                 pack.count = counter;
                 pack.type = 1;
             }
+        if (pack.type == 0)
+        {
+            
+        }
     }
     free(buffer);
     fclose(file);
