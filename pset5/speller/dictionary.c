33ff846f6d2f177b615c9bde9342d3808e3b0d56
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-01 00:30:17 +0000

    Fri, Jul 1, 2022, 12:30 AM +00:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -70,7 +70,10 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    if (load(dictionary))
+    {
+        return word_number;
+    }
     return 0;
 }
 
