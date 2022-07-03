f66cd9c0a90793a21dcddbbb4f0ff9f75dd93f43
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-03 16:21:23 +0000

    Sun, Jul 3, 2022, 4:21 PM +00:00

--- pset6/dna/tst.py
+++ pset6/dna/tst.py
@@ -8,11 +8,12 @@ dictt['Sergio']['3'] = 1
 
 d2['1'] = 1
 d2['2'] = 1
-d2['3'] = 1
+d2['3'] = 2
 
 print(dictt)
 print(d2)
 success_count = 0
-if db[name][subseq] == dict_seq[subseq]:
-    print('entrei')
-    success_count += 1
+for subseq in dictt['Sergio']:
+    if dictt['Sergio'][subseq] == d2[subseq]:
+        print('entrei')
+        success_count += 1
