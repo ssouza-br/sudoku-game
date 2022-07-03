def8c499cfafaeda0e7fda4e2c0750e197795b09
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-03 03:11:01 +0000

    Sun, Jul 3, 2022, 3:11 AM +00:00

--- pset6/readability.py
+++ pset6/readability.py
@@ -1,8 +1,15 @@
 text = input("Text: ")
 
 counter_letter = 0
+counter_words = 0
 
-//counting letters
+# counting letters
 for c in text:
     if c.isalpha():
         counter_letter += 1
+
+# counting words
+for word in text.split(" "):
+    counter_words += 1
+
+print(counter_letter, counter_words)
