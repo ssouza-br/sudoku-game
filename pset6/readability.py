df8e0eeb2930a9206cbffceed94af16c2cec8dd8
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-03 13:28:20 +0000

    Sun, Jul 3, 2022, 1:28 PM +00:00

--- pset6/readability.py
+++ pset6/readability.py
@@ -2,14 +2,19 @@ text = input("Text: ")
 
 counter_letter = 0
 counter_words = 0
+counter_sentences = 0
 
-# counting letters
+
 for c in text:
+    # counting letters
     if c.isalpha():
         counter_letter += 1
+    # counting words
+    if c == " ":
+        counter_words += 1
+    # counting sentences
+    if c == "." or c == "!" or c == "?":
+        counter_sentences+=1
 
-# counting words
-for word in text.split(" "):
-    counter_words += 1
 
-print(counter_letter, counter_words)
+
