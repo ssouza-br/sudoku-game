4b3c5df660042dc93ff1aa3c20f70ffa59ee89ce
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-03 02:11:46 +0000

    Sun, Jul 3, 2022, 2:11 AM +00:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,4 +1,3 @@
-
 while True:
     try:
         height = int(input("Height: "))
@@ -7,8 +6,10 @@ while True:
     if height > 0 and height < 9:
         break
 
+
 def blocks(n):
-    print(" "*(height - n) + "#"*n + "  " +"#"*n)
+    print(" "*(height - n) + "#"*n + "  " + "#"*n)
+
 
 for i in range(1, height+1):
     blocks(i)
