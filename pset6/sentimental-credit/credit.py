53d3d523466c225685691e7aec36a87517a36bd0
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-03 02:27:03 +0000

    Sun, Jul 3, 2022, 2:27 AM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -13,6 +13,76 @@ def card_type(n):
     else:
         return "INVALID"
 
+long int gen_decimals(int n)
+{
+    long int res = 1;
+    int i = n;
+    while (i > 0)
+    {
+        res = res * 10;
+        i--;
+    }
+    return res;
+}
 
+int get_digits(long int n, int pos)
+{
+    return (n % gen_decimals(pos) - n % gen_decimals(pos - 1)) / gen_decimals(pos - 1);
+}
+
+int len_number(long int n)
+{
+    long int res = 1;
+    int i = 0;
+    while (res >= 0)
+    {
+        res = n - gen_decimals(i);
+        i++;
+    }
+    return i - 1;
+}
+
+int check_sum_part1(long int n)
+{
+    int sum = 0;
+    for (int i = 2; i <= len_number(n); i = i + 2)
+    {
+        int double_digts = 2 * get_digits(n, i);
+        if (len_number(double_digts) > 1)
+        {
+            sum = sum + get_digits(double_digts, 1) + get_digits(double_digts, 2);
+        }
+        else
+        {
+            sum = sum + double_digts;
+        }
+    }
+    return sum;
+}
+
+int check_sum_part2(long int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= len_number(n); i = i + 2)
+    {
+        sum = sum + get_digits(n, i);
+    }
+    return sum;
+}
+
+bool card_validation(long int n)
+{
+    bool valid = false;
+    int sum = check_sum_part1(n) + check_sum_part2(n);
+    if (get_digits(sum, 1) == 0)
+    {
+        valid = true;
+        return valid;
+    }
+    else
+    {
+        return valid;
+    }
+}
 
 main()
