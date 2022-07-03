dc3e851f761cc5a22e8aaaca14bac890bdce6483
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-03 02:59:38 +0000

    Sun, Jul 3, 2022, 2:59 AM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -1,19 +1,22 @@
 number = input("Number: ")
 
+
 def main():
     print(card_type(number))
+
 
 def card_type(n):
     if card_validation:
         if (len(n) == 13 or len(n) == 16) and n[0] == "4":
             return "VISA"
-        elif len(n) == 16 and n[0] == "5" and (int(n[1]) in range(1,6)):
+        elif len(n) == 16 and n[0] == "5" and (int(n[1]) in range(1, 6)):
             return "MASTERCARD"
-        elif len(n) == 15  and n[0] == "3" and (n[1] == "4" or n[1] == "7"):
+        elif len(n) == 15 and n[0] == "3" and (n[1] == "4" or n[1] == "7"):
             return "AMEX"
         else:
             return "INVALID"
     return "INVALID"
+
 
 def card_validation(n):
     sum = 0
@@ -24,13 +27,12 @@ def card_validation(n):
         else:
             sum += 2*int(n[i])
 
-
     for i in range(len(n), 0, -2):
         sum += int(n[i])
-
 
     if sum % 10 == 0:
         return True
     return False
+
 
 main()
