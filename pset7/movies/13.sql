0e2afeecd1329e331ba97979e953ffe6ca1e2393
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 23:57:03 +0000

    Tue, Jul 5, 2022, 11:57 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,7 +1,5 @@
-SELECT DISTINCT(name) , movies.title FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
-WHERE movies.title IN
+SELECT DISTINCT(name) FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.movie_id=movies.id
+WHERE NOT people.name='Kevin Bacon' AND movies.title IN
 
 (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
-WHERE (people.name='Kevin Bacon') AND (people.birth=1958));
-
--- SELECT name, birth FROM people WHERE name='Kevin Bacon' and birth=1958;
+WHERE (people.name='Kevin Bacon') AND (people.birth=1958))
--- pset7/movies/tst.sql
+++ pset7/movies/tst.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT(name) , movies.title, stars.person_id FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.movie_id=movies.id
-WHERE movies.title IN
+SELECT DISTINCT(name) FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.movie_id=movies.id
+WHERE NOT people.name='Kevin Bacon' AND movies.title IN
 
 (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
 WHERE (people.name='Kevin Bacon') AND (people.birth=1958)) ORDER BY movies.title ASC
