c4858a51605827ee799583d42a6a8527a80fbe72
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 00:47:15 +0000

    Tue, Jul 5, 2022, 12:47 AM +00:00

--- pset7/movies/8.sql
+++ pset7/movies/8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN movies ON people.id=movies.id WHERE movies.title='Toy Story'
+SELECT name FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id WHERE movies.title='Toy Story';
