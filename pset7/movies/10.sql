77a2c92d3ac7c82503580962896dfb7b55c9bd08
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 22:00:28 +0000

    Tue, Jul 5, 2022, 10:00 PM +00:00

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT(name) FROM people JOIN directors on people.id=directors.person_id JOIN movies ON directors.movie_id=movies.id JOIN ratings ON movies.id=ratings.movie_id WHERE ratings.rating>=9.0;
+SELECT name FROM people JOIN directors on people.id=directors.person_id JOIN movies ON directors.movie_id=movies.id JOIN ratings ON movies.id=ratings.movie_id WHERE ratings.rating>=9.0;
--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id WHERE people.name='Chadwick Boseman' AND people.name='Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
+SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id WHERE people.name='Johnny Depp' AND people.name='Helena Bonham Carter';
