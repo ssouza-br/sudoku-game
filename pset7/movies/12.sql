ec276f6cb05b6a5a98aaf58b2dad9cba0579f14a
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 22:15:06 +0000

    Tue, Jul 5, 2022, 10:15 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,5 +1,5 @@
 (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
 WHERE (people.name='Johnny Depp'))
-
+INNER JOIN
 (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
 WHERE (people.name='Helena Bonham Carter'));
