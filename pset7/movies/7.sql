4060119dc9186dba3cb0357885f36d3fcf7b086f
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 00:35:33 +0000

    Tue, Jul 5, 2022, 12:35 AM +00:00

--- pset7/movies/7.sql
+++ pset7/movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, ratings.rating FROM movies JOIN (SELECT * FROM ratings ORDER BY rating ASC) ON id=movie_id WHERE year=2010;
+SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id=ratings.movie_id WHERE movies.year=2010 ORDER BY ratings.rating ASC ;
