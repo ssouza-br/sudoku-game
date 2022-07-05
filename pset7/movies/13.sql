cf2665b64db10a14899e72934bee09e8f70727ef
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 22:57:55 +0000

    Tue, Jul 5, 2022, 10:57 PM +00:00

---
+++ pset7/movies/13.sql
@@ -0,0 +1,5 @@
+SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
+WHERE (people.name='Kevin Bacon')
+INTERSECT
+SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
+WHERE (people.name='Helena Bonham Carter');
