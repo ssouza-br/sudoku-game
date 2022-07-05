dce0a410479118b274332b890d632e6c7b8424cf
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 21:56:48 +0000

    Tue, Jul 5, 2022, 9:56 PM +00:00

---
+++ pset7/movies/12.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id WHERE people.name='Chadwick Boseman' AND people.name='Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
