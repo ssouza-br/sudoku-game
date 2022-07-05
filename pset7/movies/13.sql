7e3f5a617955d507bd5fb2a3ab80098ff299523f
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 23:36:30 +0000

    Tue, Jul 5, 2022, 11:36 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,4 +1,4 @@
-SELECT name , movies.title FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
+SELECT DISTINCT(name) , movies.title FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
 WHERE movies.title IN
 
 (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
