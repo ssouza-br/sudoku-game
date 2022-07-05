fc984062b648d171a016458a8e27b23b1f3f8f3c
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 23:17:59 +0000

    Tue, Jul 5, 2022, 11:17 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,3 +1,3 @@
 SELECT name FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
 WHERE movies.title IN (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
-WHERE (people.name='Kevin Bacon'));
+WHERE (people.name='Kevin Bacon') AND (people.birth=1958));
