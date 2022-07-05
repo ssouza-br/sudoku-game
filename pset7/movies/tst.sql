a878c5b3ca6a6dc95a77a929270cb08825065439
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 23:47:07 +0000

    Tue, Jul 5, 2022, 11:47 PM +00:00

--- pset7/movies/tst.sql
+++ pset7/movies/tst.sql
@@ -1,8 +1,8 @@
 -- SELECT DISTINCT(name) , movies.title, stars.person_id FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
 -- WHERE movies.title IN
 
--- (SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
--- WHERE (people.name='Kevin Bacon') AND (people.birth=1958))
+-- SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
+-- WHERE (people.name='Kevin Bacon') AND (people.birth=1958)
 
-SELECT DISTINCT(name) , movies.title, stars.person_id, people.birth FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
-WHERE movies.title='End of the Line'
+SELECT name FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.person_id=movies.id
+WHERE movies.title='Diner'
