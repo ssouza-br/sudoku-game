40b9e17fd2127d67bbc4b4886f79c52e3475bbf8
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 01:09:13 +0000

    Tue, Jul 5, 2022, 1:09 AM +00:00

--- pset7/movies/8.sql
+++ pset7/movies/8.sql
@@ -1,19 +1,1 @@
 SELECT name FROM people JOIN stars ON people.id=stars.person_id JOIN movies ON stars.movie_id=movies.id WHERE movies.title='Toy Story';
-
-
-
--- Example query:
--- select column_names
--- from Student_detail as s join Attendance_details as a on
--- s.s_id = a.s_id join Batch_details as b on
--- s.s_id = b.s_id;
-
-
-
-
-
-
--- SELECT name FROM people WHERE id=(SELECT person_id FROM stars JOIN movies on stars.movie_id=movies.id WHERE movies.title='Toy Story')
--- SELECT person_id FROM stars JOIN movies on stars.movie_id=movies.id WHERE movies.title='Toy Story'
-
--- SELECT name FROM people WHERE id=(SELECT person_id FROM stars JOIN movies on stars.movie_id=movies.id WHERE movies.title='Toy Story')
