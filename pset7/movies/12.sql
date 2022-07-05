61d1517384d3913525e62a04219a2df8974d2a07
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-05 22:54:12 +0000

    Tue, Jul 5, 2022, 10:54 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,3 +1,6 @@
 SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
-WHERE (people.name='Johnny Depp') AND (people.name='Helena Bonham Carter');
+WHERE (people.name='Johnny Depp')
+INTERSECT
+SELECT title FROM movies JOIN stars ON movies.id=stars.movie_id JOIN people ON stars.person_id=people.id
+WHERE (people.name='Helena Bonham Carter');
 
