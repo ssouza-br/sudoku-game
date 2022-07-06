ee88808e70ebae037d55e593e1a34732bf06c026
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-06 23:18:14 +0000

    Wed, Jul 6, 2022, 11:18 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT * FROM crime_scene_reports WHERE day=28 AND month=07 AND year=2021 AND street='Humphrey Street'
+-- SELECT * FROM crime_scene_reports WHERE day=28 AND month=07 AND year=2021 AND street='Humphrey Street'
 
-SELECT * FROM interviews
+SELECT * FROM interviews WHERE day=28 AND month=07 AND year=2021
