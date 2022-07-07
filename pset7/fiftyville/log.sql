ceaa33b67022dda664f213e947a80eebc3cbcad4
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-07 00:21:56 +0000

    Thu, Jul 7, 2022, 12:21 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -9,7 +9,6 @@
 -- | 297 | 2021 | 7     | 28  | Humphrey Street | Littering took place at 16:36. No known witnesses.                                                                                                                                                                       |
 -- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 
-
 -- cheking interviews
 -- SELECT * FROM interviews WHERE day=28 AND month=07 AND year=2021 AND transcript LIKE "%bakery%"
 -- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
@@ -20,6 +19,5 @@
 -- | 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 -- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 
-
 --check first interview -> security camera
---SELECT * FROM bakery_security_logs WHERE day=28 AND month=07 AND year=2021 --AND transcript LIKE "%bakery%"
+SELECT * FROM bakery_security_logs WHERE day=28 AND month=07 AND year=2021 --AND transcript LIKE "%bakery%"
