b5095823670268c7fec6b34a8bd9b98e368c49a9
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-07 01:27:57 +0000

    Thu, Jul 7, 2022, 1:27 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -156,4 +156,6 @@
 
 --check license_plate with security camera
 SELECT * FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=29 AND month=07 AND year=2021 AND duration<=60)
+
+
 WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=07 AND year=2021 AND activity='exit' AND hour=10)
