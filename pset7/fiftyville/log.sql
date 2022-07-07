b74cbed6913caa63c39b6c83c089368b41257044
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-07 22:54:15 +0000

    Thu, Jul 7, 2022, 10:54 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,39 +1,39 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- checking crimes on 288/07/2021 at Humphrey Street
--- SELECT * FROM crime_scene_reports WHERE day=28 AND month=07 AND year=2021 AND street='Humphrey Street'
--- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
--- | id  | year | month | day |     street      |                                                                                                       description                                                                                                        |
--- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
--- | 295 | 2021 | 7     | 28  | Humphrey Street | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
--- | 297 | 2021 | 7     | 28  | Humphrey Street | Littering took place at 16:36. No known witnesses.                                                                                                                                                                       |
--- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+    -- SELECT * FROM crime_scene_reports WHERE day=28 AND month=07 AND year=2021 AND street='Humphrey Street'
+    -- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+    -- | id  | year | month | day |     street      |                                                                                                       description                                                                                                        |
+    -- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+    -- | 295 | 2021 | 7     | 28  | Humphrey Street | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
+    -- | 297 | 2021 | 7     | 28  | Humphrey Street | Littering took place at 16:36. No known witnesses.                                                                                                                                                                       |
+    -- +-----+------+-------+-----+-----------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 
 -- cheking interviews
--- SELECT * FROM interviews WHERE day=28 AND month=07 AND year=2021 AND transcript LIKE "%bakery%"
--- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
--- | id  |  name   | year | month | day |                                                                                                                                                     transcript                                                                                                                                                      |
--- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
--- | 161 | Ruth    | 2021 | 7     | 28  | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
--- | 162 | Eugene  | 2021 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
--- | 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
--- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+    -- SELECT * FROM interviews WHERE day=28 AND month=07 AND year=2021 AND transcript LIKE "%bakery%"
+    -- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+    -- | id  |  name   | year | month | day |                                                                                                                                                     transcript                                                                                                                                                      |
+    -- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+    -- | 161 | Ruth    | 2021 | 7     | 28  | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+    -- | 162 | Eugene  | 2021 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
+    -- | 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+    -- +-----+---------+------+-------+-----+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 
 --check first interview -> security camera
--- SELECT * FROM bakery_security_logs WHERE day=28 AND month=07 AND year=2021 AND activity='exit' AND hour=10
--- +-----+------+-------+-----+------+--------+----------+---------------+
--- | id  | year | month | day | hour | minute | activity | license_plate |
--- +-----+------+-------+-----+------+--------+----------+---------------+
--- | 260 | 2021 | 7     | 28  | 10   | 16     | exit     | 5P2BI95       |
--- | 261 | 2021 | 7     | 28  | 10   | 18     | exit     | 94KL13X       |
--- | 262 | 2021 | 7     | 28  | 10   | 18     | exit     | 6P58WS2       |
--- | 263 | 2021 | 7     | 28  | 10   | 19     | exit     | 4328GD8       |
--- | 264 | 2021 | 7     | 28  | 10   | 20     | exit     | G412CB7       |
--- | 265 | 2021 | 7     | 28  | 10   | 21     | exit     | L93JTIZ       |
--- | 266 | 2021 | 7     | 28  | 10   | 23     | exit     | 322W7JE       |
--- | 267 | 2021 | 7     | 28  | 10   | 23     | exit     | 0NTHK55       |
--- | 268 | 2021 | 7     | 28  | 10   | 35     | exit     | 1106N58       |
--- +-----+------+-------+-----+------+--------+----------+---------------+
+    -- SELECT * FROM bakery_security_logs WHERE day=28 AND month=07 AND year=2021 AND activity='exit' AND hour=10
+    -- +-----+------+-------+-----+------+--------+----------+---------------+
+    -- | id  | year | month | day | hour | minute | activity | license_plate |
+    -- +-----+------+-------+-----+------+--------+----------+---------------+
+    -- | 260 | 2021 | 7     | 28  | 10   | 16     | exit     | 5P2BI95       |
+    -- | 261 | 2021 | 7     | 28  | 10   | 18     | exit     | 94KL13X       |
+    -- | 262 | 2021 | 7     | 28  | 10   | 18     | exit     | 6P58WS2       |
+    -- | 263 | 2021 | 7     | 28  | 10   | 19     | exit     | 4328GD8       |
+    -- | 264 | 2021 | 7     | 28  | 10   | 20     | exit     | G412CB7       |
+    -- | 265 | 2021 | 7     | 28  | 10   | 21     | exit     | L93JTIZ       |
+    -- | 266 | 2021 | 7     | 28  | 10   | 23     | exit     | 322W7JE       |
+    -- | 267 | 2021 | 7     | 28  | 10   | 23     | exit     | 0NTHK55       |
+    -- | 268 | 2021 | 7     | 28  | 10   | 35     | exit     | 1106N58       |
+    -- +-----+------+-------+-----+------+--------+----------+---------------+
 
 --check second interview -> atm_transactions
 -- SELECT * FROM atm_transactions WHERE day=28 AND month=07 AND year=2021 AND atm_location LIKE "%Humphrey%" AND transaction_type='withdraw'
