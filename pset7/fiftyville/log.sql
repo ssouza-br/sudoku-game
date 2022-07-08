1621f19c3b4b0c68c9013fbd8b0fc91a39eb89af
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-08 23:48:20 +0000

    Fri, Jul 8, 2022, 11:48 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -355,3 +355,6 @@
 
 --checking if caller and receiver are in the same flight
     SELECT id, name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=07 AND year=2021 AND duration<=60)
+
+
+SELECT COUNT(*) FROM movies JOIN (SELECT * FROM ratings WHERE rating=10.0) ON id=movie_id;
