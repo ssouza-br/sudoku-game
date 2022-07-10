50742cd7e4daef2b2b0f42720e1a2a66c88249e1
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-10 16:40:05 +0000

    Sun, Jul 10, 2022, 4:40 PM +00:00

--- pset4/volume/volume.c
+++ pset4/volume/volume.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     while (fread(&buffer, 1, 2, input) == 2)
     {
-
+        fwrite(factor*&buffer, 1, 2, output);
     }
 
     // Close files
