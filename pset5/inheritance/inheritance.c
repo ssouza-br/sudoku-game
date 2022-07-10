4f82dc5bed6ab7373845dc602eeba76ca78238a6
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-10 17:38:43 +0000

    Sun, Jul 10, 2022, 5:38 PM +00:00

--- pset5/inheritance/inheritance.c
+++ pset5/inheritance/inheritance.c
@@ -50,10 +50,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents = {parent0, parent1};
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles = {random_allele(), random_allele()};
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
 
     }
 
@@ -61,10 +63,12 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents = NULL;
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        p->alleles = {random_allele(), random_allele()};
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
 
     }
 
