c2dca31157c8510c653d88399d63db795cdff7a5
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-10 01:24:31 +0000

    Sun, Jul 10, 2022, 1:24 AM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -6,6 +6,8 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
+char LETTERS[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','T','W','U','V','X','Z'};
+
 int compute_score(string word);
 
 int main(void)
@@ -28,8 +30,11 @@ int compute_score(string word)
     {
         for (int j = 0; j < 26; j++)
         {
-            if (word[i]==)
-            score +=
+            if (word[i]==LETTER[j])
+            {
+                score += POINTS[j];
+            }
         }
     }
+    return score;
 }
