0c42728385475351aed355b2bd945645516b5411
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-15 22:59:04 +0000

    Fri, Jul 15, 2022, 10:59 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -123,6 +123,12 @@ def register():
         hash = generate_password_hash(request.form.get("password"))
         register = request.form.get("register")
 
+        if not username:
+            return apology("must provide username to registrated", 403)
+
+        if not hash:
+            return apology("must provide password to registrated", 403)
+
         if register:
             db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
             # Redirect user to home page
