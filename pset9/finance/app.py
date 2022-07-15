a99901db51d94722e0da0e1e69d15a83c9a9b660
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-15 22:40:04 +0000

    Fri, Jul 15, 2022, 10:40 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -128,7 +128,7 @@ def register():
             #db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
             db.execute("INSERT INTO users (username, hash) VALUES ('triste', 'senha')")
             # Redirect user to home page
-        return redirect("/")
+        #return redirect("/")
         # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
