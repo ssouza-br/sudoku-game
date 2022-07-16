b1174d4557a9fc2803c9f866c72099d89b61c3e3
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-16 15:47:21 +0000

    Sat, Jul 16, 2022, 3:47 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -119,7 +119,7 @@ def quote():
             return apology("must provide symbol to quote", 403)
 
         dict_res = lookup(symbol)
-        if symbol != None:
+        if dict_res != None:
             # Redirect user to home page
             return render_template("quoted.html", dict_res=dict_res)
         else:
