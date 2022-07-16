cff3559af1243e627eb53d21f8810c44f033c7f3
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-16 15:16:23 +0000

    Sat, Jul 16, 2022, 3:16 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -121,7 +121,7 @@ def quote():
         dict_res = lookup(symbol)
         print(dict_res)
         # Redirect user to home page
-        return redirect("/")
+        return redirect("/quoted.html", )
         # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("quote.html")
