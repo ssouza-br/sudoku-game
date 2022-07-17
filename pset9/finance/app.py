8107c2f2da2ed2d39787ac3f6006b00e44020df1
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-17 15:10:30 +0000

    Sun, Jul 17, 2022, 3:10 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -84,7 +84,8 @@ def buy():
                 "INSERT INTO transactions (users_id, symbol, name, price, quantity) VALUES (?, ?, ?, ?, ?)"
                 , session["user_id"], dict_res['symbol'], dict_res['name'], dict_res['price'], dict_res['qty'])
 
-            res = db.execute("SELECT * FROM transactions ")
+            res = db.execute("SELECT * FROM transactions")
+            print(res)
             return render_template("receipt.html", dict_res=dict_res)
         else:
             return apology("You don't have money enough to buy these shares")
