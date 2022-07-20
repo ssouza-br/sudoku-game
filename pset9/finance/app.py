d82dd082a660a5317b33caab53a9f8cb7ead22f1
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-20 00:18:46 +0000

    Wed, Jul 20, 2022, 12:18 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -250,7 +250,7 @@ def sell():
         if qty < 0:
             return apology("must provide positive quatitity of shares to buy", 400)
 
-        current_qty = db.execute("SELECT quantity FROM transactions WHERE users_id = ? and symbol = ?", session["user_id"], symbol.upper())
+        current_qty = db.execute("SELECT quantity FROM transactions WHERE users_id = ? and symbol = ?", session["user_id"], symbol)
         current_qty = current_qty[0]['quantity']
         if qty > current_qty:
             return apology("don't try to sell more that you have", 400)
