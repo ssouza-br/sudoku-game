c29fe91ae1ff6baaf324134f7ca3588d1457fc5e
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-21 00:03:40 +0000

    Thu, Jul 21, 2022, 12:03 AM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -25,7 +25,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///sudoku.db")
 
 # History list
 
