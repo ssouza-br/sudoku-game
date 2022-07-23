4ddf963343f9f54e345861acc57768fd9eb6c545
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-23 23:12:58 +0000

    Sat, Jul 23, 2022, 11:12 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -68,10 +68,17 @@ def game():
             #     # print('val', val)
             #     # print('digitado', json_res[int(key[2])-1][str(key[:2])])
 
+        json_answ = db.execute("SELECT * FROM current_games WHERE COD_MATRIZ = ? AND USERS_ID = ?",
+                               dict_res['game_number'], session["user_id"])
+
         for key in dict_res:
             if key!='game_number':
+                
                 db.execute("UPDATE current_games SET {}=? WHERE ORDEM=? AND COD_MATRIZ=? AND USERS_ID=?".format(
                     key[:2]), dict_res[key], key[2], dict_res['game_number'], session["user_id"])
+
+
+
         json_res = db.execute( "SELECT * FROM current_games WHERE COD_MATRIZ = ? AND USERS_ID = ?", dict_res['game_number'], session["user_id"])
         print(dict_res)
         return render_template("game.html", res=json_res)
