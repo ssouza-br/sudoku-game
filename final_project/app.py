ce00ea7c3d3b4ae7097c66cb4141ad2fa1b8bf0c
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-23 23:35:11 +0000

    Sat, Jul 23, 2022, 11:35 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -68,17 +68,17 @@ def game():
             #     # print('val', val)
             #     # print('digitado', json_res[int(key[2])-1][str(key[:2])])
 
-        json_answ = db.execute("SELECT * FROM current_games WHERE COD_MATRIZ = ? AND USERS_ID = ?",
-                               dict_res['game_number'], session["user_id"])
-        print(json_answ)
-        print(json_answ[6])
-        print(dict_res)
+        json_answ = db.execute("SELECT * FROM answer_games WHERE COD_MATRIZ = ?",
+                               dict_res['game_number'])
+        # print(json_answ)
+        # print(json_answ[6])
+        # print(dict_res)
         for key in dict_res:
             if key!='game_number':
-                print('part1a: ', int(key[2])-1)
-                print('part2a:', str(key[:2]))
-                print('part1:', json_answ[int(key[2])-1][str(key[:2])])
-                print('part2:', int(dict_res[key]))
+                # print('part1a: ', int(key[2])-1)
+                # print('part2a:', str(key[:2]))
+                # print('part1:', json_answ[int(key[2])-1][str(key[:2])])
+                # print('part2:', int(dict_res[key]))
                 if int(json_answ[int(key[2])-1][str(key[:2])]) == int(dict_res[key]):
                     db.execute("UPDATE current_games SET {}=? WHERE ORDEM=? AND COD_MATRIZ=? AND USERS_ID=?".format(
                     key[:2]), dict_res[key], key[2], dict_res['game_number'], session["user_id"])
