3e30a741e43444705e83c1e071a9c065f4d9539c
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-23 23:19:28 +0000

    Sat, Jul 23, 2022, 11:19 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -73,11 +73,12 @@ def game():
 
         for key in dict_res:
             if key!='game_number':
-                
-                db.execute("UPDATE current_games SET {}=? WHERE ORDEM=? AND COD_MATRIZ=? AND USERS_ID=?".format(
+                if json_answ[int(key[2])-1][str(key[:2])] == dict_res[key]:
+                    db.execute("UPDATE current_games SET {}=? WHERE ORDEM=? AND COD_MATRIZ=? AND USERS_ID=?".format(
                     key[:2]), dict_res[key], key[2], dict_res['game_number'], session["user_id"])
-
-
+                else:
+                    db.execute("UPDATE current_games SET {}=NULL WHERE ORDEM=? AND COD_MATRIZ=? AND USERS_ID=?".format(
+                        key[:2]), key[2], dict_res['game_number'], session["user_id"])
 
         json_res = db.execute( "SELECT * FROM current_games WHERE COD_MATRIZ = ? AND USERS_ID = ?", dict_res['game_number'], session["user_id"])
         print(dict_res)
