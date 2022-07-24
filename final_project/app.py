7d8f8efbd27a81058efa04dafe543e13a1b9c9a8
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 15:55:18 +0000

    Sun, Jul 24, 2022, 3:55 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -75,6 +75,7 @@ def game():
 
         elif life <= 0:
             flash('Vc perdeu campeão!!!', 'error')
+            db.execute("DELETE FROM current_games WHERE USERS_ID = ? AND COD_MATRIZ = ?", )
             return redirect("/")
 
         json_res = db.execute( "SELECT * FROM current_games WHERE COD_MATRIZ = ? AND USERS_ID = ?", dict_res['game_number'], session["user_id"])
