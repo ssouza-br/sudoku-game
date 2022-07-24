b18b750e14da8abbd8298505e9c25e919bceec6f
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 17:25:45 +0000

    Sun, Jul 24, 2022, 5:25 PM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -36,5 +36,5 @@ for j in range(100):
 #     sudo = np.array([int(num) for num in sudo]).reshape(9, 9)
     print(sudo)
     for i in range(9):
-        db.execute("INSERT INTO new_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+        db.execute("INSERT INTO new_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
     print('jogo: ', j, 'salvo no banco')
