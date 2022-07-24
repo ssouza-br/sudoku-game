3f4dbe81cbf46193e7d3b0eb0c27202f61a9d733
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 17:00:22 +0000

    Sun, Jul 24, 2022, 5:00 PM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -16,13 +16,17 @@ db = SQL("sqlite:///sudoku.db")
 #         [6, 4, 5,  9, 7, 8,  3, 1, 2],
 #         [9, 7, 8,  3, 1, 2,  6, 4, 5]]
 
-for j in range(100):
-    sudo = generators.random_sudoku(avg_rank=150)
-    sudo = np.array(list(int(sudo))).reshape(9,9)
-    print(sudo)
-    for i in range(9):
-        db.execute("INSERT INTO new_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
-    print('jogo: ', j, 'salvo no banco')
+sudo = generators.random_sudoku(avg_rank=150)
+sudo = np.array(list(str(sudo))).reshape(9,9)
+print(sudo)
+
+# for j in range(100):
+#     sudo = generators.random_sudoku(avg_rank=150)
+#     #sudo = np.array(list(int(sudo))).reshape(9,9)
+#     print(sudo)
+#     for i in range(9):
+#         db.execute("INSERT INTO new_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+#     print('jogo: ', j, 'salvo no banco')
 # # # db.execute("UPDATE new_games SET N2 = NULL, N4 = NULL, N6 = NULL, N8 = NULL WHERE COD_MATRIZ = 1 and ORDEM = 7")
 
 
