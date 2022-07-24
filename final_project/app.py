4caf354a70263118106095a07132b70d9a149171
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 15:23:15 +0000

    Sun, Jul 24, 2022, 3:23 PM +00:00

--- final_project/app.py
+++ final_project/app.py
@@ -75,7 +75,7 @@ def game():
 
         elif life <= 0:
             print('eu entrei aqui')
-            flash('Vc perdeu campeão!!!', 'danger')
+            flash('Vc perdeu campeão!!!', 'error')
             return redirect("/")
 
         json_res = db.execute( "SELECT * FROM current_games WHERE COD_MATRIZ = ? AND USERS_ID = ?", dict_res['game_number'], session["user_id"])
