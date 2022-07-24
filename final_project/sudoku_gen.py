3e9b1486c5c630fcb522c24564cda57180ce4eaa
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 17:25:08 +0000

    Sun, Jul 24, 2022, 5:25 PM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -29,11 +29,12 @@ print(arr)
 # print(sudo[0][8])
 # print(sudo[8][8])
 
-# for j in range(100):
-#     sudo = generators.random_sudoku(avg_rank=150)
+for j in range(100):
+    sudo = np.array(
+        list(str(generators.random_sudoku(avg_rank=150)))).reshape(9, 9)
 #     sudo = np.array(list(str(sudo)))
 #     sudo = np.array([int(num) for num in sudo]).reshape(9, 9)
-#     print(sudo)
-#     for i in range(9):
-#         db.execute("INSERT INTO new_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
-#     print('jogo: ', j, 'salvo no banco')
+    print(sudo)
+    for i in range(9):
+        db.execute("INSERT INTO new_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+    print('jogo: ', j, 'salvo no banco')
