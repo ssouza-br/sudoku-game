036a51e1c84ece7793a998c036d5b9b48276ebc0
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 22:18:51 +0000

    Sun, Jul 24, 2022, 10:18 PM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -44,10 +44,11 @@ print(ls2)
 # # print(sudo[0][8])
 # # print(sudo[8][8])
 
-# for j in range(10):
-#     sudo = np.array(list(str(generators.random_sudoku(avg_rank=150)))).reshape(9, 9)
-#     sudo = sudo.tolist()
-#     print(sudo)
-#     for i in range(9):
-#         db.execute("INSERT INTO new_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
-#     print('jogo: ', j, 'salvo no banco')
+for j in range(10):
+    sudo = np.array(list(str(generators.random_sudoku(avg_rank=150)))).reshape(9, 9)
+    sudo = sudo.tolist()
+    sudo = ['' if lst2[i][j] == '0' else lst2[i][j] for j in range(9) for i in range(9)]
+    print(sudo)
+    for i in range(9):
+        db.execute("INSERT INTO new_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+    print('jogo: ', j, 'salvo no banco')
