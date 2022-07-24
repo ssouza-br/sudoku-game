4036c3b496d5c414c5bced4f2d204420df7743f9
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 16:21:07 +0000

    Sun, Jul 24, 2022, 4:21 PM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -1,5 +1,8 @@
 from cs50 import SQL
+from dokusan import generator
 
+arr = generator.random_sudoku(avg_rank=150)
+print(arr)
 db = SQL("sqlite:///sudoku.db")
 
 sudo = [[1, 2, 3,  4, 5, 6,  7, 8, 9],
@@ -14,10 +17,10 @@ sudo = [[1, 2, 3,  4, 5, 6,  7, 8, 9],
         [6, 4, 5,  9, 7, 8,  3, 1, 2],
         [9, 7, 8,  3, 1, 2,  6, 4, 5]]
 
-for i in range(9):
-        db.execute("INSERT INTO answer_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                   1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+# for i in range(9):
+#         db.execute("INSERT INTO answer_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+#                    1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
 
-# db.execute("UPDATE new_games SET N2 = NULL, N4 = NULL, N6 = NULL, N8 = NULL WHERE COD_MATRIZ = 1 and ORDEM = 7")
+# # db.execute("UPDATE new_games SET N2 = NULL, N4 = NULL, N6 = NULL, N8 = NULL WHERE COD_MATRIZ = 1 and ORDEM = 7")
 
 
