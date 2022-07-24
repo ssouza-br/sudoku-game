25ef27a8720b436c3324a18512ffff45e25d3978
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-24 16:45:31 +0000

    Sun, Jul 24, 2022, 4:45 PM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -1,8 +1,6 @@
 from cs50 import SQL
 from dokusan import generators
 
-
-print(arr)
 db = SQL("sqlite:///sudoku.db")
 
 # sudo = [[1, 2, 3,  4, 5, 6,  7, 8, 9],
@@ -19,8 +17,8 @@ db = SQL("sqlite:///sudoku.db")
 
 for j in range(100):
     sudo = generators.random_sudoku(avg_rank=150)
-        for i in range(9):
-                db.execute("INSERT INTO answer_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    for i in range(9):
+        db.execute("INSERT INTO answer_games(COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                         j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
 
 # # db.execute("UPDATE new_games SET N2 = NULL, N4 = NULL, N6 = NULL, N8 = NULL WHERE COD_MATRIZ = 1 and ORDEM = 7")
