ef050f5c384c6abb3d3b8603e21dbdc231b95f91
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-25 01:09:32 +0000

    Mon, Jul 25, 2022, 1:09 AM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -2,7 +2,12 @@ from cs50 import SQL
 from dokusan import generators, solvers
 import numpy as np
 
-def
+def sudo_to_list(sudo):
+    sudo = list(str(sudo))
+    sudo = ['' if sudo[i] == '0' else sudo[i] for i in range(81)]
+    arr = np.array(sudo).reshape(9, 9)
+    return arr.tolist()
+
 db = SQL("sqlite:///sudoku.db")
 
 sudo = generators.random_sudoku(avg_rank=150)
@@ -21,14 +26,12 @@ print(solution)
 
 for j in range(10):
     sudo_new = generators.random_sudoku(avg_rank=150)
-    sudo = list(str(sudo_new))
-    sudo = ['' if sudo[i] == '0' else sudo[i] for i in range(81)]
-    arr = np.array(sudo).reshape(9, 9)
-    sudo = arr.tolist()
+    sudo = sudo_to_list(sudo_new)
 
     sudo_answer = solvers.backtrack(sudo)
-
+    sudo_a = sudo_to_list(sudo_answer)
 
     for i in range(9):
         db.execute("INSERT INTO new_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+        db.execute("INSERT INTO answer_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo_a[i][0], sudo_a[i][1], sudo_a[i][2], sudo_a[i][3], sudo_a[i][4], sudo_a[i][5], sudo_a[i][6], sudo_a[i][7], sudo_a[i][8])
     print('jogo: ', j, 'salvo no banco')
