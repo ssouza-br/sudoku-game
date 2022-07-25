2858d6dd5ebb2f5585921c8e1f1bc80f7bd45549
Sérgio Souza <85451816+ssouza-br@users.noreply.github.com>
2022-07-25 00:55:34 +0000

    Mon, Jul 25, 2022, 12:55 AM +00:00

--- final_project/sudoku_gen.py
+++ final_project/sudoku_gen.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from dokusan import generators
+from dokusan import generators, solvers
 import numpy as np
 
 db = SQL("sqlite:///sudoku.db")
@@ -19,7 +19,12 @@ db = SQL("sqlite:///sudoku.db")
 # print(type(sudo))
 
 
-# sudo = generators.random_sudoku(avg_rank=150)
+sudo = generators.random_sudoku(avg_rank=150)
+print(sudo)
+solution = solvers.backtrack(sudo)
+print(solution)
+
+
 # lst = list(str(generators.random_sudoku(avg_rank=150)))
 # print(lst)
 # # print(np.array([[1,2,3],[4,5,6]]))
@@ -30,12 +35,12 @@ db = SQL("sqlite:///sudoku.db")
 # ls2 = ['' if lst2[i][j] == '0' else lst2[i][j] for j in range(9) for i in range(9)]
 # print(ls2)
 
-sudo = list(str(generators.random_sudoku(avg_rank=150)))
-sudo = ['' if sudo[i] == '0' else sudo[i] for i in range(81)]
-arr = np.array(sudo).reshape(9,9)
-sudo = arr.tolist()
-#sudo = sudo.tolist()
-print(sudo)
+# sudo = list(str(generators.random_sudoku(avg_rank=150)))
+# sudo = ['' if sudo[i] == '0' else sudo[i] for i in range(81)]
+# arr = np.array(sudo).reshape(9,9)
+# sudo = arr.tolist()
+# #sudo = sudo.tolist()
+# print(sudo)
 
 # arr = np.array(list(str(generators.random_sudoku(avg_rank=150))))
 
@@ -51,12 +56,12 @@ print(sudo)
 # # print(sudo[0][8])
 # # print(sudo[8][8])
 
-for j in range(10):
-    sudo = list(str(generators.random_sudoku(avg_rank=150)))
-    sudo = ['' if sudo[i] == '0' else sudo[i] for i in range(81)]
-    arr = np.array(sudo).reshape(9, 9)
-    sudo = arr.tolist()
-    print(sudo)
-    for i in range(9):
-        db.execute("INSERT INTO new_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
-    print('jogo: ', j, 'salvo no banco')
+# for j in range(10):
+#     sudo = list(str(generators.random_sudoku(avg_rank=150)))
+#     sudo = ['' if sudo[i] == '0' else sudo[i] for i in range(81)]
+#     arr = np.array(sudo).reshape(9, 9)
+#     sudo = arr.tolist()
+#     print(sudo)
+#     for i in range(9):
+#         db.execute("INSERT INTO new_games (COD_MATRIZ, ORDEM, N1, N2, N3, N4, N5, N6, N7, N8, N9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", j+1, i+1, sudo[i][0], sudo[i][1], sudo[i][2], sudo[i][3], sudo[i][4], sudo[i][5], sudo[i][6], sudo[i][7], sudo[i][8])
+#     print('jogo: ', j, 'salvo no banco')
